c0cdf046ee1eae4e59d403d786db35e242df17f9
Olivia <olivia@Olivias-MBP.hitronhub.home>
2022-05-20 17:46:47 -0700

    Created Customer class and implemented a doGet & a doPost to test it.

---
+++ src/main/java/com/example/olifood/Servlet.java
@@ -0,0 +1,47 @@
+package com.example.olifood;
+
+
+import com.example.olifood.entity.Customer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.io.IOUtils;
+import org.json.JSONObject;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "Servlet", value = "/Servlet")
+public class Servlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json");
+        ObjectMapper mapper = new ObjectMapper();
+        Customer customer= new Customer();
+        customer.setEmail("sun@laioffer.com");
+        customer.setPassword("123456");
+        customer.setFirstName("rick");
+        customer.setLastName("sun");
+        customer.setEnabled(true);
+        response.getWriter().println(mapper.writeValueAsString(customer));
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        JSONObject jsonRequest = new JSONObject(IOUtils.toString(request.getReader()));
+        String email = jsonRequest.getString("email");
+        String firstName = jsonRequest.getString("first_name");
+        String lastName = jsonRequest.getString("last_name");
+        int age = jsonRequest.getInt("age");
+
+        System.out.println("Email is: " + email);
+        System.out.println("First name is: " + firstName);
+        System.out.println("Last name is: " + lastName);
+        System.out.println("Age is: " + age);
+
+        response.setContentType("application/json");
+        JSONObject jsonResponse = new JSONObject();
+        response.setStatus(HttpServletResponse.SC_OK);
+
+    }
+}
